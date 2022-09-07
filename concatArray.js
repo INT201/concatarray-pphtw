be9e3e6bafe45ef13a7fa5a94a3524689e0a64fe
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2022-09-07 13:20:01 +0700

    complete

--- concatArray.js
+++ concatArray.js
@@ -2,5 +2,15 @@ const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
   //code here
+  if (array1 == null && array2 == null) {
+    return undefined
+  }
+  else if (array1 == [] && array2 == []){
+    return []
+  }
+  array1 = array1 ?? []
+  array2 = array2 ?? []
+  array = array1.concat(array2)
+  return array
 }
 module.exports = concatArray
